7dfb4a399ef54874567fec44d25c5b0bd43056dd
ian-from-dover <ian09hong@gmail.com>
2022-03-01 18:01:50 +0800

    Create EventChannel Tutorial

---
+++ Assets/Scripts/EventChannelTutorial.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventChannelTutorial : MonoBehaviour
+{
+    // declare the event channel you wanna use and drag the scriptable object into the field in the unity editor
+    public EventChannel playerTeleported;
+
+    void Start()
+    {
+        // subscribe and unsubscribe methods to the eventchannel if you need the event to
+        // call your methods when the event happens
+        // if you're only calling .publish(); then you dont have to subscribe.
+        playerTeleported.OnChange += PigeonSpawned;
+        // eventName.OnChange += yourMethod;
+    }
+
+    void OnDestroy()
+    {
+        // unsubscribe event channels to prevent resource leak
+        playerTeleported.OnChange += PigeonSpawned;
+    }
+
+    // this method will be called when the event happens
+    void PigeonSpawned()
+    {
+        Debug.Log("The event has happened!");
+    }
+    
+    void MakeEventHappen()
+    {
+        // you can make the event happen in any method by simply calling .Publish() of the EventChannel
+        playerTeleported.Publish();
+    }
+}
