aed1cae2f18cc8a515e232c7e6c1a4425cb1a452
Shawn Koh <shawn@shawnkoh.sg>
2022-03-11 20:35:54 +0800

    Add PlayerTeleportedSO

---
+++ Assets/Scripts/PlayerTeleportedSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu]
+public class PlayerTeleportedSO : ScriptableObject
+{
+    public UnityAction<Vector3> OnChange;
+
+    public void Publish(Vector3 position)
+    {
+        OnChange.Invoke(position);
+    }
+}
