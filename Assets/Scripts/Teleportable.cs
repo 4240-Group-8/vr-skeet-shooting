1d0cda77a68675dba65d7e3eb5e2cd4b1e610df7
Shawn Koh <shawn@shawnkoh.sg>
2022-03-11 18:57:13 +0800

    Add basic teleport implementation

--- Assets/Scripts/Teleportable.cs
+++ Assets/Scripts/Teleportable.cs
@@ -2,37 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum TeleportState
+{
+    Resting,
+    AimingValid,
+    AimingInvalid,
+}
+
 public class Teleportable : MonoBehaviour
 {
+    public GameObject Player;
     public GameObject Indicator;
 
     public string InputIdentifier;
 
     private int _layerMask;
 
+    private TeleportState _state;
+
     private RaycastHit _raycastHit;
-    // Start is called before the first frame update
     void Start()
     {
         _layerMask = 1 << 7;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetAxis(InputIdentifier) != 1)
         {
+            switch (_state)
+            {
+                case TeleportState.Resting:
+                    // Do nothing.
+                    break;
+                case TeleportState.AimingInvalid:
+                    _state = TeleportState.Resting;
+                    break;
+                case TeleportState.AimingValid:
+                    Teleport(_raycastHit.point);
+                    _state = TeleportState.Resting;
+                    break;
+            }
+            UpdateIndicator();
             return;
         }
 
         if (Physics.Raycast((transform.position), transform.forward, out _raycastHit, Mathf.Infinity, _layerMask))
         {
-            Indicator.transform.position = _raycastHit.point;
+            _state = TeleportState.AimingValid;
             Debug.DrawRay(transform.position, transform.forward * _raycastHit.distance, Color.blue);
         }
         else
         {
+            _state = TeleportState.AimingInvalid;
             Debug.DrawRay(transform.position, transform.forward * 1000, Color.white);
+        }
+        
+        UpdateIndicator();
+    }
+
+    private void Teleport(Vector3 position)
+    {
+        if (Player == null)
+            return;
+        Player.transform.position = position;
+    }
+
+    private void UpdateIndicator()
+    {
+        switch (_state)
+        {
+            case TeleportState.AimingValid:
+                Indicator.transform.position = _raycastHit.point;
+                Indicator.SetActive(true);
+                return;
+            default:
+                Indicator.SetActive(false);
+                return;
         }
     }
 }
